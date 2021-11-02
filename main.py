3f15c8b1d35763ab3f426c0c9d1bd438c3319536
Michael Paulinus <michaelpaulinus@gmail.com>
2021-11-03 00:03:43 +0200

    Updates

--- main.py
+++ main.py
@@ -14,23 +14,20 @@ def extract_features(img):
     # create glcm of an image (distance=[1,2], angles=[0,45,90,135])
     glcm = greycomatrix(img, [1,2], [0, np.pi/4, np.pi/2, 3*np.pi/4], levels=256)
 
-    contrast = greycoprops(glcm, 'contrast')
     energy = greycoprops(glcm, 'energy')
     homogeneity = greycoprops(glcm, 'homogeneity')
+    contrast = greycoprops(glcm, 'contrast')
     dissimilarity = greycoprops(glcm, 'dissimilarity')
 
     # compute entropy
-    # p = np.zeros((256, 256, 2, 4)) # probability matrix
-    # entropy = np.zeros((2,4)) # entropy matrix
-    # for d in range(2):
-    #     for theta in range(4):
-    #         p[:,:,d,theta] = glcm[:,:,d:theta]/glcm[:,:,d:theta].sum()
-    #         entropy[d,theta] = sum([
-    #             x*(-np.log(x)) if x!=0 else 0 for x in p[:,:,d,theta].flatten()
-    #         ])
+    p = np.zeros((256, 256, 2, 4)) # probability matrix
+    entropy = np.zeros((2,4)) # entropy matrix
+    for d in range(2):
+        for theta in range(4):
+            p[:,:,d,theta] = glcm[:,:,d,theta]/glcm[:,:,d,theta].sum()
+            entropy[d,theta] = sum([x*(-np.log(x)) if x!=0 else 0 for x in p[:,:,d,theta].flatten()])
 
-    # return np.array([contrast, energy, homogeneity, dissimilarity, entropy])
-    return np.array([contrast, energy, homogeneity, dissimilarity])
+    return np.array([energy, homogeneity, contrast, dissimilarity, entropy])
 
 # store image folder into variable
 path_normal_train = glob.glob("chest_xray_dataset\\train\\NORMAL\\*.jpeg")
@@ -86,7 +83,9 @@ for img in pneumonia_img_train:
     X_train.append(features)
     y_train.append(img_class)
 
-X_train = np.array(X_train).reshape(((len(normal_img_train) + len(pneumonia_img_train)),32)) # (len(normal_img_test) + (len(pneumonia_img_test) = number of images trained, 40 = distances * orientations * number of features
+total_train = len(normal_img_train) + len(pneumonia_img_train) # total train images
+vector = 2 * 4 * 5 # distances * orientations * number of features
+X_train = np.array(X_train).reshape(total_train, vector) 
 
 ############################ HARALICK FEATURES OF TEST DATA ############################
 # extract haralick features of normal chest
@@ -103,7 +102,8 @@ for img in pneumonia_img_test:
     X_test.append(features)
     y_test.append(img_class)
 
-X_test = np.array(X_test).reshape((len(normal_img_test) + (len(pneumonia_img_test)),32)) # (len(normal_img_test) + (len(pneumonia_img_test) = number of images trained, 40 = distances * orientations * number of features
+total_test = len(normal_img_test) + len(pneumonia_img_test) # total test images
+X_test = np.array(X_test).reshape(total_test, vector)
 
 # select classifier
 clf_sel = input('Select classifier: 1=MLP, 2=NBC, 3=SVM: ')
@@ -119,16 +119,43 @@ else:
 
 # train the model
 clf.fit(X_train, y_train)
-# results
-print('############################ RESULTS ############################')
 # predict the response
 y_pred = clf.predict(X_test)
+
+# results
+print('############################ RESULTS ############################')
+normal_test = 0
+pneumonia_test = 0
+
+# determine number of normal/pneumonia images in test data
+for i in y_test:
+    if i==0:
+        normal_test = normal_test + 1
+    else:
+        pneumonia_test = pneumonia_test + 1
+
+normal_predicted_correctly = 0
+pneumonia_predicted_correctly = 0
+
+# determine number of correctly predicted images
+for i in range(0, len(y_test)):
+    if y_test[i] == y_pred[i] == 0:
+        normal_predicted_correctly = normal_predicted_correctly + 1
+    elif y_test[i] == y_pred[i] == 1:
+        pneumonia_predicted_correctly = pneumonia_predicted_correctly + 1
+
+# summary
+print('- No. of normal samples in test samples: ', normal_test)
+print('- No. of normal samples predicted correctly: ', normal_predicted_correctly)
+print('- No. of pneumonia samples in test samples: ', pneumonia_test)
+print('- No. of pneumonia samples predicted correctly: ', pneumonia_predicted_correctly)
+print("- No. of mislabeled samples out of a total %d samples : %d" % (X_test.shape[0], (y_test != y_pred).sum()))
+
 # accuracy score
 print('- Accuracy: ', metrics.accuracy_score(y_test, y_pred))
 # precision score
-print('- Precision: ', metrics.recall_score(y_test, y_pred))
-# summary
-print("- Number of mislabeled points out of a total %d points : %d" % (X_test.shape[0], (y_test != y_pred).sum()))
-print('- Test: ', y_test)
-print('- Predicted: ', y_pred)
+print('- Precision: ', metrics.precision_score(y_test, y_pred))
+
+print('\n- Test: ', y_test)
+print('\n- Predicted: ', y_pred)
 print('############################ END OF RESULTS ############################')
